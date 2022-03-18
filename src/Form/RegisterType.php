5641eca52d94eaa055df6eaeb2c959b78db83f1c
Ibrahima Cherif <ibrahimacherif963@gmail.com>
2022-03-18 03:16:05 +0100

    Valider les données grâce au composant Validator et modification de la langue en en fr à partir du fichier translation.yaml

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterType extends AbstractType
 {
@@ -19,18 +20,21 @@ class RegisterType extends AbstractType
         $builder
             ->add('firstname',TextType::class,[
                 'label' =>'Votre Prénom',
+                'constraints' => new Length(3,60),
                 'attr'=>[
                     'placeholder' => 'Merci de saisir votre Prénom'
                 ]
             ])
             ->add('lastname', TextType::class,[
                 'label' => 'Votre Nom',
+                'constraints' => new Length(3,60),
                 'attr' => [
                     'placeholder' => 'Merci de saisir votre Nom'
                 ]
             ])
             ->add('email',EmailType::class,[
                 'label' => 'Votre Email',
+                'constraints' => new Length(3,100),
                 'attr' => [
                     'placeholder' => 'Merci de saisir votre Email'
                 ]
