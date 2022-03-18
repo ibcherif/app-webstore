6a06719aee467ca42f662c54e14f4918afbcd119
Ibrahima Cherif <ibrahimacherif963@gmail.com>
2022-03-18 02:40:45 +0100

    Hasher (encoder) le mot de passe avec l'injection dependance de UserPasswordHasherInterface (symfony 6) -  UserPasswordEncoderInterface (symfony 5)

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
@@ -17,13 +18,15 @@ class RegisterController extends AbstractController
         $this->entityManager=$entityManger;
     }
     #[Route('/inscription', name: 'app_register')]
-    public function index(Request $request): Response
+    public function index(Request $request, UserPasswordHasherInterface $encoder): Response
     {
         $user=new User();
         $form=$this->createForm(RegisterType::class,$user);
         $form->handleRequest($request);
         if ($form->isSubmitted() and $form->isValid()){
             $user=$form->getData();
+            $password=$encoder->hashPassword($user, $user->getPassword());
+            $user->setPassword($password);
             $this->entityManager->persist($user);
             $this->entityManager->flush();
         }
