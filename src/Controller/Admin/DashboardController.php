ca533c09bbb3922cef4afbb91702d8ba1c866714
Ibrahima Cherif <ibrahimacherif963@gmail.com>
2022-03-20 03:45:34 +0100

    Mapping de l'entité Product() dans EasyAdmin et Création des produits dans le backoffice

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Category;
+use App\Entity\Product;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -55,6 +56,7 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Users', 'fa fa-user', User::class);
-        yield MenuItem::linkToCrud('Catégories', 'fa fa-list', Category::class);
+        yield MenuItem::linkToCrud('Category', 'fa fa-list', Category::class);
+        yield MenuItem::linkToCrud('Products', 'fa fa-tag', Product::class);
     }
 }
